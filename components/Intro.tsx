fbb770d82b5d72ddb841119f9a84a59ff3cb705c
ZHOON <zhoon95@gmail.com>
2026-04-09 10:46:57 +0900

    Revert "chore : tidy up"
    This reverts commit ca8c8f7e6d3857b4d938c2b533614861c4245bb2.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -20,6 +20,8 @@ const Intro = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const radialRef = useRef<HTMLDivElement>(null);
 
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
   useEffect(() => {
     const images = refs.current.filter(Boolean);
 
@@ -71,7 +73,7 @@ const Intro = () => {
             ref={(el) => {
               refs.current[i] = el;
             }}
-            src={`${src.src}`}
+            src={`${basePath}${src.src}`}
             alt={`Intro image ${i + 1}`}
             fill
             className="object-cover absolute inset-0"
